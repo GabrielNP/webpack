11502aede3edf8553b7604488269a29f072452af
GabrielNP <gabrielnp.2010@gmail.com>
2020-07-12 19:50:34 -0300

    Separating code from libraries in bundles.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,6 +17,11 @@ plugins.push(
     })
 );
 
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    filename: 'vendor.bundle.js'
+}));
+
 if(process.env.NODE_ENV == 'production') {
 
     plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
@@ -35,7 +40,10 @@ if(process.env.NODE_ENV == 'production') {
 }
 
 module.exports = {
-    entry: './app-src/app.js',
+    entry: {
+        app: './app-src/app.js',
+        vendor: ['bootstrap', 'jquery', 'reflect-metadata']
+    },
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'dist'),
